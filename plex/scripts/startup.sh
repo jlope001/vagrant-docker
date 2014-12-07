68296eafee669c9df3b951c75aa149d56e079e93
Jose Lopez <jose.lopez@sendgrid.com>
2014-12-07 12:44:42 -0800

    remove redundant information in startup

--- plex/scripts/startup.sh
+++ plex/scripts/startup.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-# create plex media user
-usermod -u 1000 media
-groupmod -g 1000 media
-
-# setup folders
-mkdir -p /config/plex
-chown -R media:media /config/plex
-chown -R media:media /media
-
 cd /usr/lib/plexmediaserver && HOME=/config/plex ./start.sh
