1a767c9a58d12c53c5b0d676f1f8b9f20f82f5a7
Jose Lopez <jose.lopez@sendgrid.com>
2014-12-09 22:13:39 -0800

    FINALLY getting the plex media server working had to deal with /config im assuming

--- plex/scripts/startup.sh
+++ plex/scripts/startup.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-cd /usr/lib/plexmediaserver && HOME=/config/plex ./start.sh
+# create plex media user
+usermod -u 1000 media
+groupmod -g 1000 media
+
+# setup folders
+mkdir -p /config/plex
+chown -R media:media /config/plex
+chown -R media:media /media
+
+su media -c "cd /usr/lib/plexmediaserver && HOME=/config/plex ./start.sh"
