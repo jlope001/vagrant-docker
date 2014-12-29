87501e79df13ed1b52ca9616965973832630f8f4
Jose Lopez <jose.lopez@sendgrid.com>
2014-12-28 17:39:03 -0800

    update wordpress to move to a persistent container due to rebuild removes plugins

--- wordpress/scripts/startup.sh
+++ wordpress/scripts/startup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-# update owncloud database ip if linked as this chnages based on docker
+# move over installation files
+cp -rf /var/www/wordpress/* /var/www/blog/.
+
+# fix permissions - to something more standard
+chown -R 1000:1000 /var/www/blog
+
+# update database ip if linked as this chnages based on docker
 # NOTE - replace the IP holder with __MYSQL_IP__ so it will be properly replaced
 sed -i "s~__MYSQL_IP__~"$MYSQL_PORT_3306_TCP_ADDR"~g" /var/www/blog/wp-config.php
