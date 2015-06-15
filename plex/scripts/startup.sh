a809429422381d2e7469d8ced8b227fde5a59bf0
Jose Lopez <jose.lopez@sendgrid.com>
2015-06-14 18:54:37 -0700

    update plex media server to latest version

--- plex/scripts/startup.sh
+++ plex/scripts/startup.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 # remove stale files from previous run
-rm -rf /var/run/*
 rm -f "/config/Library/Application Support/Plex Media Server/plexmediaserver.pid"
 
-# create plex media user
-usermod -u 1000 media
-groupmod -g 1000 media
-
-# setup folders
+# # setup folders
 mkdir -p /config/plex
-chown -R media:media /config/plex
-chown -R media:media /media
+mkdir -p /data
+chown -R plex:plex /config/plex
+chown -R plex:plex /data
 
-su media -c "cd /usr/lib/plexmediaserver && HOME=/config/plex ./start.sh"
+cd /usr/lib/plexmediaserver && HOME=/config/plex  ./start.sh
